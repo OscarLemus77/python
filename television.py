71962cfc7abe29b0908186709d41f6413d37388b
OscarLemus77 <olemusgonzalez@unomaha.edu>
2024-11-24 19:06:55 -0600

    Update television.py

--- television.py
+++ television.py
@@ -58,6 +58,6 @@ class Television:
         :return: tv status
         """
         if self.__muted:
-            return f"Power = {self.__status},Channel = {self.__channel},Volume = {0}"
+            return f'Power = {self.__status},Channel = {self.__channel},Volume = {Television.MIN_VOLUME}'
         else:
-            return f"Power = {self.__status},Channel = {self.__channel},Volume = {self.__volume}"
+            return f'Power = {self.__status}, Channel = {self.__channel},Volume = {self.__volume}'
